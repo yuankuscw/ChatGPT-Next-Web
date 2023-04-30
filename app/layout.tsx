c6debe6cc199ee28c182162a3a840046703a0aa8
yuankuscw <130632353+yuankuscw@users.noreply.github.com>
2023-04-30 09:53:52 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "ChatGPT 中文镜像网站",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatGPT 中文镜像网站",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
