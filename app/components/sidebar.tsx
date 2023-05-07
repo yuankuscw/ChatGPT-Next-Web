552a25fd20217b5cf2a0de965dfc6f4d2901d68f
yuankuscw <130632353+yuankuscw@users.noreply.github.com>
2023-05-07 13:19:22 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -151,7 +151,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
+            <a href="https://fk.yuankuw.com" target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
